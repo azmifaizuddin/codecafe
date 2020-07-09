b10cffd90e0f7e966886844589ecafba7d19c04c
azmifaizuddin <azmifaizudin@gmail.com>
2020-07-09 08:35:06 +0700

    Menambah Foreigen Key dan Kolom Poin
    Foreign key user_id pada questions dan answers, menambahkan juga kolom points

---
+++ database/migrations/2020_07_09_011844_add_user_id_on_questions_and_answers.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdOnQuestionsAndAnswers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::table('answers', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropForeign(['users_id']);
+            $table->dropColumn(['users_id']);
+        });
+
+        Schema::table('answers', function (Blueprint $table) {
+            $table->dropForeign(['users_id']);
+            $table->dropColumn(['users_id']);
+        });
+
+    }
+}
